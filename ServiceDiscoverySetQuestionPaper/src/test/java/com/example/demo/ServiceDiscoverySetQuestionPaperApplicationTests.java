cf90ba24928102081e269b5a8c49917e8858c548
jahanvi <jahnavi.adi@gmail.com>
2022-05-08 20:06:58 +0530

    service

--- ServiceDiscoverySetQuestionPaper/src/test/java/com/example/demo/ServiceDiscoverySetQuestionPaperApplicationTests.java
+++ ServiceDiscoverySetQuestionPaper/src/test/java/com/example/demo/ServiceDiscoverySetQuestionPaperApplicationTests.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineExamPortalApplicationTests {
+class ServiceDiscoverySetQuestionPaperApplicationTests {
 
 	@Test
 	void contextLoads() {
-	
 	}
 
 }
